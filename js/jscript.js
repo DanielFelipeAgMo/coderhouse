f44b24d906e0c0cc71fdead72865702d5434bb6d
Daniel Felipe A.M <danielagudelo87@gmail.com>
2021-07-02 22:12:24 -0500

    agoritmos js

---
+++ js/jscript.js
@@ -0,0 +1,28 @@
+//declaro variables
+/*let a = 8;
+let b = 7;
+
+//Impresion de lo solicitado por consola
+/*console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//Impresion mediante un alert
+/*alert("Las varables a & b tienen los valres " + a +" y " + b + " correspondientemente.");
+
+alert("La suma de a + b es igual a  " + a + b);
+alert("La resta de a - b es igual a  " + a - b);
+alert("La multiplicación de a * b es igual a  " + a * b);
+alert("La división de a / b es igual a  " + a / b);
+alert("El sobrante de a % b es igual a  " + a % b);
+
+
+*/
+
+//Algoritmo con Condicional
+
+/*let edad = prompt("Ingresa tu edad por favor :")
+
+if (edad == "") { alert('No haz ingresado tu edad') } else if (edad > 0 && edad <= 15) { alert('Eres un niño mejor toma tu biberón') } else if (edad >= 16 && edad < 18) { alert('Espera un poco ya casi puedes ingresar') } else if (edad >= 18 && edad <= 50) { alert('Puedes ingresar') } else if (edad >= 51 && edad <= 90) { alert('Puedes ingresar pero ten cuidado con tu corazón, puede darte un infarto') } else if (edad >= 91 && edad <= 100) { alert('Puedes ingresar...pero a un asilo') } else { alert('Eres un fosil viviente') }
